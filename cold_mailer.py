6a3ccc5cfbc99179697bfcd48d4d8b1820acf088
Itamar Golan <golan.itamar@gmail.com>
2023-04-22 20:32:20 +0300

    Update cold_mailer.py

--- cold_mailer.py
+++ cold_mailer.py
@@ -9,26 +9,26 @@ from langchain.agents.agent_toolkits import ZapierToolkit
 from langchain.llms import OpenAI
 from langchain.utilities.zapier import ZapierNLAWrapper
 
-llm = OpenAI(temperature=0)
-zapier = ZapierNLAWrapper()
 
+llm = OpenAI(temperature=0.7)
+
+zapier = ZapierNLAWrapper()
 toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
-llm = OpenAI(temperature=0.9)
 
 tools = load_tools(["serpapi"], llm=llm)
 
 agent = initialize_agent(tools + toolkit.get_tools(), llm, agent="zero-shot-react-description", verbose=True)
 
-st.title('Send cold emails with AI')
+st.title('ColdMailer')
 
-product = st.text_input('Name of the product youre selling: ') # Launchman
-function = st.text_input('What does the product do: ') # Automate your content marketing
-prospect = st.text_input('Who is the person youre selling to: ') # Sukh from Launchman
+product = st.text_input('What is the Name of the product youre selling: ') # Banana
+function = st.text_input('What does your product do: ') # Automatic cold mails engine
+prospect = st.text_input('Who is the person youre selling to: ') # Elon Musk
 
 if st.button('Do the magic!'):
   template = """
-  # You are Marko, a cold email outreach expert selling {product} with the function {function}.
-  Search for a person named {prospect} and craft a cold email with 3 paragraphs that contains introduction about them, how {product} can help them, and book a meeting. Do not label the paragraphs, make sure to start a new line after each paragraph. Send it as email to: sunnyashiin@gmail.com."""
+  # You are Coldy, a cold email outreach expert which is selling {product} with the function {function}.
+  Search for a person called {prospect} and craft a cold email with 3 paragraphs that contains introduction about them, how {product} can help them, and book a meeting. Do not label the paragraphs, make sure to start a new line after each paragraph. Send it as email to: elon.musk@gmail.com."""
 
   prompt = PromptTemplate(
       input_variables=["product", "function", "prospect"],
